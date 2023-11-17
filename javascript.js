1b1b3ed0b896e04f47587728a77e04b61758749a
Irfan Yilmaz <iy.usa@outlook.com>
2023-11-17 04:23:36 -0500

    edit the password requirements not met error msg

--- javascript.js
+++ javascript.js
@@ -33,7 +33,7 @@ addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    passwordReqMsg = "<b>Doesnt Meet Requirements:</b><i><br>6-12 characters, atleast 1 digit. At least one uppercase letter</i>";
+    passwordReqMsg = "<b>Doesnt Meet Requirements:</b><i><br>6 to 12 characters. Atleast 1 digit. At least 1 uppercase letter</i>";
     passwordInput.addEventListener("focusout", (e) => {
         passText = e.target.value;
         if(!password_regex.test(passText))
